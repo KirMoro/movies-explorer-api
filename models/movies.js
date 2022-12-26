a1a732d16eb3839760ed7e2667be58d9e28342f5
Kirill Morozov <morozov.ks@gmail.com>
2022-12-26 23:34:53 +0300

    feat: add models users and movies

---
+++ models/movies.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+import { urlRegex } from './users.js';
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  director: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    minLength: 2,
+  },
+  year: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => urlRegex.test(link),
+      message: () => 'Требуется http(s) ссылка',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => urlRegex.test(link),
+      message: () => 'Требуется http(s) ссылка',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => urlRegex.test(link),
+      message: () => 'Требуется http(s) ссылка',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+    minLength: 1,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { versionKey: false });
+
+export const Movies = mongoose.model('movie', movieSchema);
